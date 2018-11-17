306e2c7bece4ad222b7bde1aaca96afff1b16b5d
Michał Oziero <michaloziero@o2.pl>
2018-11-17 09:18:09 +0100

    Dodanie reszty aktywności

---
+++ app/src/main/java/com/example/michaloziero/plaxcrm/DodajFirmyActivity.java
@@ -0,0 +1,13 @@
+package com.example.michaloziero.plaxcrm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DodajFirmyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dodaj_firmy);
+    }
+}
---
+++ app/src/main/java/com/example/michaloziero/plaxcrm/EdycjaUmowyActivity.java
@@ -0,0 +1,13 @@
+package com.example.michaloziero.plaxcrm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EdycjaUmowyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edycja_umowy);
+    }
+}
